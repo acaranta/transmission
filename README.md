9964e0f408e7f4aaeab839e6c40218d6bf6183b6
acaranta <arthur@caranta.com>
2024-03-31 17:29:18 +0200

    applied all changfes from recent dpersons version

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-[![logo](http://blogmmix.ch/sites/default/files/imagecache/gross/6/transmission-bittorrent1.png)](https://www.transmissionbt.com/)
+[![logo](https://raw.githubusercontent.com/dperson/transmission/master/logo.png)](https://www.transmissionbt.com/)
 
 # Transmission
 
@@ -14,23 +14,41 @@ a cross-platform back-end.
 This Transmission container was built to automatically download a level1 host
 filter (can be used with dperson/openvpn).
 
-    sudo docker run --cap-add=NET_ADMIN --device /dev/net/tun --name vpn \
+    sudo docker run -it --cap-add=NET_ADMIN --device /dev/net/tun --name vpn \
                 --dns 8.8.4.4 --dns 8.8.8.8 --restart=always \
                 -d dperson/openvpn-client ||
-    sudo docker run --name bit --net=container:vpn \
+    sudo docker run -it --name bit --net=container:vpn \
                 -d dperson/transmission
-    sudo docker run --name web -p 80:80 -p 443:443 --link vpn:bit \
+    sudo docker run -it --name web -p 80:80 -p 443:443 --link vpn:bit \
                 -d dperson/nginx -w "http://bit:9091/transmission;/transmission"
+
+**NOTE**: The default username/password are `admin`/`admin`. See `TRUSER` and
+`TRPASSWD` below, for how to change them.
+
+**NOTE2**: To connect to the transmission container, point your browser to the
+actual `<hostname_or_IP_address>` of the system running docker with a URI as
+below:
+
+    https://<hostname_or_IP_address>/transmission/web/
+
+**NOTE3**: To open the peer connection port add the following to the
+`docker run` command:
+
+    -p 51413:51413 -p 51413:51413/udp
 
 ## Hosting a Transmission instance
 
-    sudo docker run --name transmission -p 9091:9091 -d dperson/transmission
+    sudo docker run -it --name transmission -p 9091:9091 -d dperson/transmission
 
-OR set local storage:
+OR set local storage (see *Complex configuration* below):
 
-    sudo docker run --name transmission -p 9091:9091 \
-                -v /path/to/directory:/var/lib/transmission-daemon/downloads \
+    sudo docker run -it --name transmission -p 9091:9091 \
+                -v /path/to/directory:/var/lib/transmission-daemon \
                 -d dperson/transmission
+
+**NOTE**: The configuration is in `/var/lib/transmission-daemon/info`, downloads
+are in `/var/lib/transmission-daemon/downloads`, and partial downloads are in
+`/var/lib/transmission-daemon/incomplete`.
 
 ## Configuration
 
@@ -39,30 +57,46 @@ OR set local storage:
     Usage: transmission.sh [-opt] [command]
     Options (fields in '[]' are optional, '<>' are required):
         -h          This help
+        -n          No auth config; don't configure authentication at runtime
         -t ""       Configure timezone
                     possible arg: "[timezone]" - zoneinfo timezone for container
 
     The 'command' (if provided and valid) will be run instead of transmission
 
-ENVIROMENT VARIABLES (only available with `docker run`)
+ENVIRONMENT VARIABLES
 
  * `TRUSER` - Set the username for transmission auth (default 'admin')
  * `TRPASSWD` - Set the password for transmission auth (default 'admin')
- * `TIMEZONE` - As above, set a zoneinfo timezone, IE `EST5EDT`
+ * `TZ` - Configure the zoneinfo timezone, IE `EST5EDT`
+ * `USERID` - Set the UID for the app user
+ * `GROUPID` - Set the GID for the app user
+
+Other environment variables beginning with `TR_` will edit the configuration
+file accordingly:
+
+ * `TR_MAX_PEERS_GLOBAL=400` will translate to `"max-peers-global": 400,`
 
 ## Examples
 
 Any of the commands can be run at creation with `docker run` or later with
-`docker exec transmission.sh` (as of version 1.3 of docker).
+`docker exec -it transmission transmission.sh` (as of version 1.3 of docker).
 
-    sudo docker run --name transmission -d dperson/transmission -t EST5EDT
+### Setting the Timezone
 
-Will get you the same settings as
+    sudo docker run -it --name transmission -e TZ=EST5EDT \
+                -d dperson/transmission
 
-    sudo docker run --name transmission -p 9091:9091 -d dperson/transmission
-    sudo docker exec transmission transmission.sh -T EST5EDT \
-                ls -AlF /etc/localtime
-    sudo docker restart transmission
+## Complex configuration
+
+If you wish to adapt the default configuration, use something like the following
+to copy it from a running container:
+
+    sudo docker cp transmission:/var/lib/transmission-daemon /some/path
+
+You can use the modified configuration with:
+
+    sudo docker run -it --name transmission -p 9091:9091 \
+                -v /some/path:/var/lib/transmission-daemon -d dperson/transmission
 
 # User Feedback
 
--- transmission.sh
+++ transmission.sh
@@ -18,28 +18,63 @@
 
 set -o nounset                              # Treat unset variables as an error
 
-dir="/dl"
+dir="/var/lib/transmission-daemon"
+[[ -d $dir/downloads ]] || mkdir -p $dir/downloads
+[[ -d $dir/incomplete ]] || mkdir -p $dir/incomplete
+[[ -d $dir/info/blocklists ]] || mkdir -p $dir/info/blocklists
+[[ -r $dir/info/settings.json ]] || cat >$dir/info/settings.json <<-EOF
+	{
+	    "blocklist-enabled": 0,
+	    "dht-enabled": true,
+	    "download-dir": "/var/lib/transmission-daemon/downloads",
+	    "incomplete-dir": "/var/lib/transmission-daemon/incomplete",
+	    "incomplete-dir-enabled": true,
+	    "download-limit": 100,
+	    "download-limit-enabled": 0,
+	    "encryption": 1,
+	    "max-peers-global": 200,
+	    "peer-port": 51413,
+	    "peer-socket-tos": "lowcost",
+	    "pex-enabled": 1,
+	    "port-forwarding-enabled": 0,
+	    "queue-stalled-enabled": true,
+	    "ratio-limit-enabled": true,
+	    "rpc-authentication-required": 1,
+	    "rpc-password": "transmission",
+	    "rpc-port": 9091,
+	    "rpc-username": "transmission",
+	    "rpc-whitelist": "127.0.0.1",
+	    "upload-limit": 100,
+	    "upload-limit-enabled": 0
+	}
+	EOF
+
 ### timezone: Set the timezone for the container
 # Arguments:
 #   timezone) for example EST5EDT
 # Return: the correct zoneinfo file will be symlinked into place
 timezone() { local timezone="${1:-EST5EDT}"
     [[ -e /usr/share/zoneinfo/$timezone ]] || {
-        echo "ERROR: invalid timezone specified" >&2
+        echo "ERROR: invalid timezone specified: $timezone" >&2
         return
     }
 
-    ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
+    if [[ -w /etc/timezone && $(cat /etc/timezone) != $timezone ]]; then
+        echo "$timezone" >/etc/timezone
+        ln -sf /usr/share/zoneinfo/$timezone /etc/localtime
+        dpkg-reconfigure -f noninteractive tzdata >/dev/null 2>&1
+    fi
 }
 
 ### usage: Help
 # Arguments:
 #   none)
 # Return: Help text
-usage() { local RC=${1:-0}
+usage() { local RC="${1:-0}"
     echo "Usage: ${0##*/} [-opt] [command]
 Options (fields in '[]' are optional, '<>' are required):
     -h          This help
+    -n          No auth config; don't configure authentication at runtime
     -t \"\"       Configure timezone
                 possible arg: \"[timezone]\" - zoneinfo timezone for container
 
@@ -48,23 +83,43 @@ The 'command' (if provided and valid) will be run instead of transmission
     exit $RC
 }
 
-cd /tmp
-
-while getopts ":ht:" opt; do
+while getopts ":hnt:" opt; do
     case "$opt" in
         h) usage ;;
-        t) timezone "$OPTARG" ;;
+        n) export NOAUTH=true ;;
+        t) timezone $OPTARG ;;
         "?") echo "Unknown option: -$OPTARG"; usage 1 ;;
         ":") echo "No argument value for option: -$OPTARG"; usage 2 ;;
     esac
 done
 shift $(( OPTIND - 1 ))
 
-[[ "${TIMEZONE:-""}" ]] && timezone "$TIMEZONE"
+[[ "${USERID:-""}" =~ ^[0-9]+$ ]] && usermod -u $USERID -o transmission
+[[ "${GROUPID:-""}" =~ ^[0-9]+$ ]]&& groupmod -g $GROUPID -o transmission
+[[ "${TZ:-""}" ]] && timezone $TZ
+for env in $(printenv | grep '^TR_'); do
+    name="$(cut -c4- <<< ${env%%=*} | tr '_A-Z' '-a-z')"
+    val="\"${env##*=}\""
+    [[ "$val" =~ ^\"([0-9]+|false|true)\"$ ]] && val="$(sed 's|"||g' <<< $val)"
+    sed -i 's|\([0-9A-Za-z"]\)$|\1,|' $dir/info/settings.json
+    if grep -q "\"$name\"" $dir/info/settings.json; then
+        sed -i "/\"$name\"/s|:.*|: $val,|" $dir/info/settings.json
+    else
+        sed -i "/^}/i\    \"$name\": $val," $dir/info/settings.json
+    fi
+    sed -rzi 's/,([^,]*)$/\1/' $dir/info/settings.json
+done
 
+watchdir="$(awk -F'=' '/"watch-dir"/ {print $2}' $dir/info/settings.json |
+            sed 's/[,"]//g')"
 [[ -d $dir/downloads ]] || mkdir -p $dir/downloads
 [[ -d $dir/incomplete ]] || mkdir -p $dir/incomplete
 [[ -d $dir/info/blocklists ]] || mkdir -p $dir/info/blocklists
+[[ $watchdir && ! -d $watchdir ]] && mkdir -p $watchdir
+
+chown -Rh transmission. $dir 2>&1 | grep -iv 'Read-only' || :
+# Ensure su is owned by root to avoid setgid issues when using user namespaces in daemon
+chown root:root /bin/su
 
 if [[ $# -ge 1 && -x $(which $1 2>&-) ]]; then
     exec "$@"
@@ -74,18 +129,17 @@ elif [[ $# -ge 1 ]]; then
 elif ps -ef | egrep -v 'grep|transmission.sh' | grep -q transmission; then
     echo "Service already running, please restart container to apply changes"
 else
-    curl -Ls 'http://list.iblocklist.com/?list=bt_level1&fileformat=p2p&archiveformat=gz' |
-                gzip -cd > $dir/info/blocklists/bt_level1
-    grep -q peer-socket-tos $dir/info/settings.json ||
-        sed -i '/"peer-port"/a \
-    "peer-socket-tos": "lowcost",' $dir/info/settings.json
-    sed -i '/"queue-stalled-enabled"/s/:.*/: true,/' $dir/info/settings.json
-#    sed -i '/"speed-limit-up"/s/:.*/: 10,/' $dir/info/settings.json
-#    sed -i '/"speed-limit-up-enabled"/s/:.*/: true,/' $dir/info/settings.json
-    exec su -l root -s /bin/bash -c "exec transmission-daemon \
-                --config-dir $dir/info --blocklist --encryption-preferred \
-                --log-error -e /dev/stdout --global-seedratio 2.0 --dht \
-                --incomplete-dir $dir/incomplete --paused --auth --foreground \
-                --username '${TRUSER:-admin}' --password '${TRPASSWD:-admin}' \
-                --download-dir $dir/downloads --no-portmap --allowed \\* 2>&1"
+    if [[ -z $(find $dir/info/blocklists/bt_level1 -mmin -1080 2>&-) && \
+                "${BLOCKLIST:-""}" != "no" ]]; then
+        # Initialize blocklist
+        url='http://list.iblocklist.com'
+        curl -Ls "$url"'/?list=bt_level1&fileformat=p2p&archiveformat=gz' |
+                    gzip -cd >$dir/info/blocklists/bt_level1
+        chown transmission. $dir/info/blocklists/bt_level1
+    fi
+    exec su -l transmission -s /bin/bash -c "exec transmission-daemon \
+                --allowed \\* --blocklist --config-dir $dir/info \
+                --foreground --log-error --no-portmap \
+                $([[ ${NOAUTH:-""} ]] && echo '--no-auth' || echo "--auth \
+                --username ${TRUSER:-admin} --password ${TRPASSWD:-admin}")"
 fi
